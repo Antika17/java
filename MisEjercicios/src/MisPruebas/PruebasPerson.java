ea97defd3d3ffb4ecff911142137fcb51283b52a
Antika17 <123806809+Antika17@users.noreply.github.com>
2023-02-06 18:40:59 +0100

    Add files via upload

---
+++ MisEjercicios/src/MisPruebas/PruebasPerson.java
@@ -0,0 +1,19 @@
+package MisPruebas;
+
+import Mipoo.Fecha;
+import Mipoo.Person;
+
+public class PruebasPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+Fecha f1=new Fecha(1,3,2020);
+Person p1=new Person("Angela", "Camino los hoyos", "48980", "Cali", f1);
+
+
+
+System.out.println(p1);
+	}
+
+}
